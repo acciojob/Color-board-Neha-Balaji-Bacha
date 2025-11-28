78a00868e7e22ccd1b92fd8f21669ddb10a3b8da
Neha-Balaji-Bacha <neha170603@gmail.com>
2025-11-28 23:14:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 //your JS code here. If required.
-const container = document.getElementById("boxContainer");
+const container = document.getElementById("boxcontainer");
 for(let i=0;i<800;i++){
 	const box = document.createElement("div");
 	box.className = "square";
 	box.addEventListener("mouseover", () => {
 		box.style.backgroundcolor="red";
+		setTimeout(()=> {
+			box.style.backgroundcolor = "black";
+		},1000);
 	});
 	container.appendChild(box);
 }
