a808366b2535b0dc6c44d04d08ad7f68d758f975
Neha-Balaji-Bacha <neha170603@gmail.com>
2025-12-02 15:48:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ for (let i = 0; i < 800; i++) {
   const box = document.createElement("div");
   box.className = "square";
   box.addEventListener("mouseover", () =>{
-	  const color = `hsl(${Math.random() * 360},70% , 50%)`;
-	  box.style.backgroundColor = 'rgb(29, 29, 29)';
+	  const color = `hsl(${Math.random() * 360}, 70%, 50%)`;
+      box.style.backgroundColor = color;
   })
   box.addEventListener("mouseout", () =>{
 	  box.style.backgroundColor = 'rgb(29, 29, 29)';
