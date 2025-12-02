3a87b2d4c66803c0a471c66adb1d10d0be748e97
Neha-Balaji-Bacha <neha170603@gmail.com>
2025-12-02 15:46:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ const container = document.getElementById("boxContainer");
 for (let i = 0; i < 800; i++) {
   const box = document.createElement("div");
   box.className = "square";
-
-  box.addEventListener("mouseenter", () => {
-    box.style.backgroundColor = "red";     // change color on hover
-    setTimeout(() => {
-      box.style.backgroundColor = "black"; // remove after 1 sec
-    }, 1000);
-  });
-
-  container.appendChild(box);
+  box.addEventListener("mouseover", () =>{
+	  const color = `hsl(${Math.random() * 360},70% , 50%)`;
+	  box.style.backgroundColor = 'rgb(29, 29, 29)';
+  })
+  box.addEventListener("mouseout", () =>{
+	  box.style.backgroundColor = 'rgb(29, 29, 29)';
+  })
+	container.appendChild(box);
 }
